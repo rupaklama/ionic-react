bad9902def09e5616ba5b4dbaef6afaa90c87ed6
Rupak Lama <rlama@uptopcorp.com>
2021-03-08 11:51:10 -0500

    making our app to Full Ionic App

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// importing ionic button component
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTitle,
+  IonToast,
+  IonToolbar,
+} from '@ionic/react';
+
+// ionic icons
+import { play as playIcon } from 'ionicons/icons';
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+
+  const handleClick = () => {
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 1500);
+  };
+
   return (
-    <div>
-      <header>
-        <h1>My App</h1>
-      </header>
-      <main>
-        <p>Add some content here…</p>
-      </main>
-    </div>
+    // Full Ionic App meaning where the Root Element is an Ion App Component
+    // we always have Two Components inside of Ion App component - IonHeader & IonContent
+    <IonApp>
+      <IonHeader>
+        {/* Inside header we typically have an Ion Toolbar like Toolbar in Mobile App */}
+        <IonToolbar>
+          <IonTitle>My App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      {/* ion-padding is one of those Optional CSS we imported in index.js */}
+      <IonContent className='ion-padding'>
+        <IonButton color='warning' onClick={handleClick}>
+          {/* IonIcon to add icon, 
+          slot prop is to where to display relative to the button - start, end & icon-only
+        */}
+          <IonIcon icon={playIcon} slot='start' />
+          Click Me
+        </IonButton>
+
+        {/** Toast Component displays a simple message by popping up at the bottom of the screen */}
+        <IonToast isOpen={showToast} message='Hello world!' />
+      </IonContent>
+    </IonApp>
   );
 }
 
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,7 +1,36 @@
+// NOTE - we can also set the Default Style Programmatically if we want to
+// import { setupConfig } from '@ionic/react';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+// NOTE - we need all the CSS below to make Full Ionic App
+// Full Ionic App meaning where the Root Element is an Ion App Component -  <IonApp>
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+// we can call this function before we tell react to render our app
+// setupConfig({
+//   // mode is ios or md (material design for android)
+//   mode: 'ios',
+// });
+// NOTE - This is really helpful for testing only
+// If we do so, we will use main benefit of IONIC - Adaptive Styling Feature
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
